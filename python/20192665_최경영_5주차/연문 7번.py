41781571495f9f71416f75a93a05780f201ff91a
KyeongYeongDEV <91273433+KyeongYeongDEV@users.noreply.github.com>
2022-03-31 11:41:19 +0900

    2022.03

---
+++ python/20192665_최경영_5주차/연문 7번.py
@@ -0,0 +1,38 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+list = []
+
+color = input("색상 #1을 입력하시오 : ")
+list.append(color)
+
+color = input("색상 2을 입력하시오 : ")
+list.append(color)
+
+color = input("색상 #3을 입력하시오 : ")
+list.append(color)
+
+t.up()
+t.goto(-100,0)
+t.down()
+t.fillcolor(list[0])
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+t.up()
+t.goto(0,0)
+t.down()
+t.fillcolor(list[1])
+t.begin_fill()
+t.circle(50)
+t.end_fill()
+
+t.up()
+t.goto(100,0)
+t.down()
+t.fillcolor(list[2])
+t.begin_fill()
+t.circle(50)
+t.end_fill()
