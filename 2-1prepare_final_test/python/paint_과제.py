2536dd6d9bee192398893868bed17ad86ba7d4e5
KyeongYeongDEV <yeong7098@naver.com>
2022-05-25 23:14:22 +0900

    Done

--- 2-1prepare_final_test/python/paint_과제.py
+++ 2-1prepare_final_test/python/paint_과제.py
@@ -1,21 +1,43 @@
 from tkinter import*
+window = Tk()
 
-mycolor = "black"
+#빨,녹,노,파. 전체 지우기, 
+mycolor = "white"
 
 def paint(event):
     x1,y1 = (event.x-1),(event.y+1)
     x2,y2 = (event.x-1),(event.y+1)
     canvas.create_oval(x1,y1,x2,y2,fill = mycolor)
 
-def change_color():
+def red_color():
     global mycolor
-    mtcolor = "red"
+    mycolor = "red"
 
-window = Tk()
+def green_color():
+    global mycolor
+    mycolor = "green"
+
+def blue_color():
+    global mycolor
+    mycolor = "blue"
+
+def yellow_color():
+    global mycolor
+    mycolor = "yellow"
+
+def delete_():
+    canvas.delete("all")
+
+
+
 canvas = Canvas(window)
-canvas.pack()
+canvas.grid(row=0,column=0,columnspan=5)
 canvas.bind("<B1-Motion>",paint)
-button = Button(window, text="빨간색", command = change_color)
-button.pack()
+
+button1= Button(window, text="빨간색", command = red_color).grid(row=1, column=0)
+button2 = Button(window, text = "녹색", command =green_color).grid(row=1, column=1)
+button3 = Button(window, text = "파란색", command = blue_color).grid(row=1, column=2)
+button4 = Button(window,text = "노란색", command =yellow_color).grid(row=1, column=3)
+button5 = Button(window,text = "전체 지우기", command = delete_).grid(row=1, column=4)
 
 window.mainloop()
--- python/20192665_최경영_7주차 과제/연문 6번.py
+++
@@ -1,6 +0,0 @@
-import random
-
-for i in range(3):
-    one = random.randint(1,6)
-    two = random.randint(1,6)
-    print("첫 번째 주사위= ",one,"두 번째 주사위 = ",two)
