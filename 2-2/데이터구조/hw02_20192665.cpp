f54d885a06c534dc0be006ab72796f9f1f114b7f
KyeongYeongDEV <yeong7098@naver.com>
2022-10-01 18:39:56 +0900

    Done

--- 2-2/데이터구조/hw02_20192665.cpp
+++ 2-2/데이터구조/hw02_20192665.cpp
@@ -26,37 +26,38 @@ bool is_empty(Stack *s){
     else{return false;}
 }
 
-// void push(Stack *s, int item){
-//     if(::is_full(s)){
-//         cout<< "ERROR_STACKFULL\n";
-//     }
-//     else{
-//         s->arr[s->top++] = item;    
-//     }    
-// }
+void push(Stack *s, int item){
+    if(::is_full(s)){
+        cout<< "ERROR_STACKFULL\n";
+    }
+    else{
+        s->arr[s->top++] = item;    
+    }    
+}
 
-// int pop(Stack *s){
-//     if(::is_empty(s)){
-//         cout<< "ERROR_STACKEMPTY\n";
-//     }
-//     else{
-//         s->top--;
-//         int tmp = s->arr[s->top];
+int pop(Stack *s){
+    if(::is_empty(s)){
+        cout<< "ERROR_STACKEMPTY\n";
+        return 0;
+    }
+    else{
+        s->top--;
+        int tmp = s->arr[s->top];
         
 
-//         return tmp;
-//     }
-// }
+        return tmp;
+    }
+}
 
-// int peek(Stack s){
-//     if(::is_empty(&s)){
-//         cout<< "ERROR_STACKEMPTY\n";
-//         return 0;
-//     }
-//     else{
-//         return s.arr[s.top-1];
-//     }
-// }
+int peek(Stack s){
+    if(::is_empty(&s)){
+        cout<< "ERROR_STACKEMPTY\n";
+        return 0;
+    }
+    else{
+        return s.arr[s.top-1];
+    }
+}
 
 // 큐 -----------------------------------------
 
@@ -84,8 +85,8 @@ int dequeque(Stack *s){
     }
     else{
         int tmp = s->arr[0];
-
         int arr[s->top];
+
         for(int i=0; i < s->top-1; i++){
             arr[i] = s->arr[i+1];
         }
@@ -93,12 +94,14 @@ int dequeque(Stack *s){
         for(int i=0; i< s->top; i++){
             s->arr[i] = arr[i];
         }
+
         s->top--;
+
         return tmp;
     }
 }
 
-int peek(Stack *s){
+int peek2(Stack *s){
     if(::is_empty(s)){
           cout <<"ERROR_QUEUEEMPTY"<<endl;
           return 0;
@@ -115,34 +118,46 @@ void printQueue(Stack *s){
     }
 }
 
+/*
+3번 문제
+단점
+1. 배열을 사용하기 때문에 저장공간의 낭비가 생깁니다.
+2. 원형 큐를 구현할 수 없습니다
+
+해결법
+양뱡향 링키드 리스트를 이용하여 구현하면 위의 문제점들이 해결이 됩니다.
+
+
+*/
+
 int main(){
     //스택 예시
 
-    // Stack s = create(2);
+    Stack s = create(2);
 
-    // cout <<pop(&s) <<endl;
+    cout <<"pop :"<<pop(&s) <<endl;
 
-    // push(&s,6);
-    // push(&s,5);
-    // push(&s,4);
+    push(&s,6);
+    push(&s,5);
+    push(&s,4);
     
-    // cout <<peek(s) <<endl;
+    cout <<"peek :"<<peek(s) <<endl;
 
-    // cout <<pop(&s) <<endl;
-    // cout <<pop(&s) <<endl;
+    cout <<"pop :"<<pop(&s) <<endl;
+    cout <<"pop :"<<pop(&s) <<endl;
 
 
     //큐 예시
-    Stack s = create(2);
+    Stack q = create(2);
 
-    enqueue(&s,10);
-    enqueue(&s,9);
-    enqueue(&s,8);
+    enqueue(&q,10);
+    enqueue(&q,9);
+    enqueue(&q,8);
 
-    cout<< "peek :"<< peek(&s)<<endl;
-    cout <<"dequeque :"<< dequeque(&s)<<endl;
-    cout <<"dequeque :"<< dequeque(&s)<<endl;
-    cout <<"dequeque :"<< dequeque(&s)<<endl;
+    cout<< "peek2 :"<< peek2(&q)<<endl;
+    cout <<"dequeque :"<< dequeque(&q)<<endl;
+    cout <<"dequeque :"<< dequeque(&q)<<endl;
+    cout <<"dequeque :"<< dequeque(&q)<<endl;
 
 
     init(&s);
@@ -151,11 +166,9 @@ int main(){
     
     printQueue(&s);
 
-    cout<< "peek :"<< peek(&s)<<endl;
+    cout<< "peek2 :"<< peek2(&s)<<endl;
     cout <<"dequeque :"<< dequeque(&s)<<endl;
     cout <<"dequeque :"<< dequeque(&s)<<endl;
-
-
 
     return 0;  
 }
