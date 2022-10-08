0f56b0de20de578f69c9a78c756534861ba5e7fb
KyeongYeongDEV <yeong7098@naver.com>
2022-10-08 23:48:29 +0900

    Done

--- 2-2/데이터구조/원형 연결리스트.cpp
+++ 2-2/데이터구조/원형 연결리스트.cpp
@@ -1,59 +1,76 @@
-#include<cstdio>
-#include<cstdlib>
+#include<iostream>
+using namespace std;
 
 typedef int element;
-typedef struct ListNode {
-	element data;
-	struct ListNode* link;
-}ListNode;
+struct Node{
+    element data;
+    struct Node *link;
+};
 
-void printList(ListNode* head) {
-	ListNode* p;
+Node* insert_last(Node *head, element data){
+    Node *tmp = (Node*)malloc(sizeof(Node));
+    tmp->data = data;
 
-	if (head == NULL) return;
-	p = head->link;
-	do {
-		printf("%d->", p->data);
-		p = p->link;
-	} while (p != head);
-	printf("%d->", p->data);
+    if(head == NULL){
+        head= tmp;
+    }
+    else{
+        Node *horse = head->link;
+
+        while(horse != NULL){
+            horse = horse->link;
+        }
+        
+        horse = tmp;
+        head = horse;
+    }
+    return head;
 }
 
-ListNode* insertFirst(ListNode* head, element data) {
-	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
-	node->data = data;
-	if (head == NULL) {
-		head = node;
-		node->link = head;
-	}
-	else {
-		node->link = head->link;
-		head->link = node;
-	}
-	return head;
-}
-ListNode* insertLast(ListNode* head, element data) {
-	ListNode* node = (ListNode*)malloc(sizeof(ListNode));
-	node->data = data;
-	if (head == NULL) {
-		head = node;
-		node->link = head;
-	}
-	else {
-		node->link = head->link;
-		head->link = node;
-		head = node;
-	}
-	return head;
+
+Node* insert_first(Node *head, int data){ //리스트의 시작 부분에 항목을 삽입하는 함수
+    Node *tmp = (Node*)malloc(sizeof(Node));
+    tmp->data = data;
+
+    if(head == NULL){
+        head = tmp;
+        head->link = head;
+    }
+    else{
+        tmp->link = head->link;
+        head->link = tmp;
+    }
+
+    return head;
 }
 
-int main(void) {
-	ListNode* head = NULL;
+void print_list(Node *head){
+    Node *tmp = head;
 
-	head = insertLast(head, 20);
-	head = insertLast(head, 30);
-	head = insertLast(head, 40);
-	head = insertFirst(head, 10);
-	printList(head);
-	return 0;
+    if(tmp == NULL){
+        cout << "NULL";
+    }
+    else{
+        cout << tmp->data<< "-> ";
+        tmp = tmp->link;
+        while(tmp != head){
+            cout << tmp->data<< "-> ";
+            tmp = tmp->link;
+        }   
+    }
+    
+    cout<<endl;
+}
+
+int main(){
+    Node *head = NULL;
+
+    // head = insert_last(head, 20);
+    // head = insert_last(head, 30);
+    // head = insert_last(head, 40);
+    head = insert_first(head, 10);
+    head = insert_first(head, 20);
+    head = insert_first(head, 30);
+
+    print_list(head);
 }
