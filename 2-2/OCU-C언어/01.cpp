afe4caeaf68b670b6f1e3449023b10bde9ad6909
KyeongYeongDEV <yeong7098@naver.com>
2022-11-05 20:05:29 +0900

    Done

--- 2-2/OCU-C언어/01.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("HELLO This is my first C program. \n\r");                                           //이 프로그램에 대한 설명
-    printf("최경영: 인제대학교 컴퓨터공학과 학번 20192665 goes for a great C programmer.\n\r"); //학생에 대한 기본 정보를 출력
-    return (0);
-}
